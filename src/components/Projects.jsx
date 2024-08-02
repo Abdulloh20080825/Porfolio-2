1d2427eabdb276c5dc9eb3d646faf6379f0b89db
Abdulloh20080825 <abdullohkurbonov2008@gmail.com>
2024-08-02 17:33:13 +0500

    Porfolio Project page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import './Projects.css'
+import React from 'react';
+import './Projects.css';
+import quran from '../img/quran.png';
+import pokemon from '../img/pokemon.png';
+import youtube from '../img/youtbe.png';
+import weather from '../img/weather.png';
 
 const Projects = () => {
-  return (
-    <div>Projects</div>
-  )
-}
+	return (
+		<div className='projects_content'>
+			<h1>My Great Projects</h1>
+			<div className='project-items'>
+				<div className='project-item'>
+					<p>Quran Application</p>
+					<img src={quran} alt='Quran Application' width={400} />
+				</div>
+				<div className='project-item'>
+					<p>Pokemon Application</p>
+					<img src={pokemon} alt='Pokemon Application' width={400} />
+				</div>
+				<div className='project-item'>
+					<p>Youtube Clone</p>
+					<img src={youtube} alt='Youtube Application' width={400} />
+				</div>
+				<div className='project-item'>
+					<p>Weather Application</p>
+					<img src={weather} alt='Weather Application' width={400} />
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default Projects
+export default Projects;
