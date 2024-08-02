c243b56a67b407f12884d1a0f825feed40cafa0f
Abdulloh20080825 <abdullohkurbonov2008@gmail.com>
2024-08-02 17:36:24 +0500

    aa

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,28 +4,37 @@ import quran from '../img/quran.png';
 import pokemon from '../img/pokemon.png';
 import youtube from '../img/youtbe.png';
 import weather from '../img/weather.png';
+import { Link } from 'react-router-dom';
 
 const Projects = () => {
 	return (
 		<div className='projects_content'>
 			<h1>My Great Projects</h1>
 			<div className='project-items'>
-				<div className='project-item'>
-					<p>Quran Application</p>
-					<img src={quran} alt='Quran Application' width={400} />
-				</div>
-				<div className='project-item'>
-					<p>Pokemon Application</p>
-					<img src={pokemon} alt='Pokemon Application' width={400} />
-				</div>
-				<div className='project-item'>
-					<p>Youtube Clone</p>
-					<img src={youtube} alt='Youtube Application' width={400} />
-				</div>
-				<div className='project-item'>
-					<p>Weather Application</p>
-					<img src={weather} alt='Weather Application' width={400} />
-				</div>
+				<Link to={'https://abdulloh-quran-app.vercel.app/'}>
+					<div className='project-item'>
+						<p>Quran Application</p>
+						<img src={quran} alt='Quran Application' width={400} />
+					</div>
+				</Link>
+				<Link to={'https://pokemon-app-five-chi.vercel.app/'}>
+					<div className='project-item'>
+						<p>Pokemon Application</p>
+						<img src={pokemon} alt='Pokemon Application' width={400} />
+					</div>
+				</Link>
+				<Link to={'https://abu-tube.vercel.app/'}>
+					<div className='project-item'>
+						<p>Youtube Clone</p>
+						<img src={youtube} alt='Youtube Application' width={400} />
+					</div>
+				</Link>
+				<Link to={'https://weather-react-js-kohl.vercel.app/'}>
+					<div className='project-item'>
+						<p>Weather Application</p>
+						<img src={weather} alt='Weather Application' width={400} />
+					</div>
+				</Link>
 			</div>
 		</div>
 	);
